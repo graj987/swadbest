a94c7fc3cb4df84a6667b85d2a833296cf0b7dd1
Mrgautam-git <gsharmapcmail9852@gmail.com>
2025-12-19 19:28:23 +0530

    forgot

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -7,7 +7,7 @@ import {
   CardHeader,
   CardTitle,
   CardDescription,
-} from "@/components/ui/card";
+} from "@/components/ui/card.jsx";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
